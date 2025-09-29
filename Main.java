2431f0312d83b22914c663207b26c048ed93a7e6
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-09-29 21:46:42 +0000

    classwork

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Give two words");
+    String word1 = scan.nextLine();
+    String word2 = scan.nextLine();
+
+    System.out.println("Give a number");
+    int N = scan.nextInt();
+    lastFirstN(word1, word2, N);
+
+
+    System.out.println("Give me two words");
+    String word3 = scan.nextLine();
+    String word4 = scan.nextLine();
+    stringManip(word3, word4);
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,14 +24,31 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    String output;
+    String firstN;
+    String lastN;
+
+    firstN = s2.substring(0, n);
+    lastN = s1.substring(s1.length() - n);
+
+    output = lastN + firstN;
+    System.out.println(output);
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
-  
+    String new1;
+    String firstLetter;
+    String lastLetters;
+
+    new1 = s1.toUpperCase();
+
+    firstLetter = s2.substring(0, 1);
+    lastLetters = s2.substring(1);
+
+    System.out.println(new1 + "and" + firstLetter.toUpperCase() + lastLetters);
   }
 
   /** Precondition: s1 and s2 are not null
